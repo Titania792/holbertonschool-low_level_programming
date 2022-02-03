3300be64ef3882203af48710a6316a89d954a1c9
Titania792 <darkwarrior792@gmail.com>
2022-02-03 10:34:45 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long int longType;
 	long long int longlongType;
 
-		printf("Size of a char %zu byte(s)\n", sizeof(charType));
-		printf("Size of an int %zu byte(s)\n", sizeof(intType));
-		printf("Size of a long init %zu byte(s)\n", sizeof(longType));
-		printf("Size of a long long init %zu byte(s)\n", sizeof(longlongType));
-		printf("Size of a float %zu byte(s)\n", sizeof(floatType));
+		printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+		printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+		printf("Size of a long init: %zu byte(s)\n", sizeof(longType));
+		printf("Size of a long long init: %zu byte(s)\n", sizeof(longlongType));
+		printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 			return (0);
 }
