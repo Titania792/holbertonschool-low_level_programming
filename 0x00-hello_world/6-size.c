9f2f87eee53799a6c05deffae83f040c3f9ab3cd
Titania792 <darkwarrior792@gmail.com>
2022-02-03 10:23:02 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 		printf("Size of a char %lu\n byte(s)\n", sizeof(char));
 		printf("Size of an int %lu\n byte(s)\n", sizeof(int));
-		printf("Size of a long init %lu\n byte(s)\n", sizeof(long init));
-		printf("Size of a long long init %lu\n byte(s)\n", sizeof(long long init));
+		printf("Size of a long init %lu\n byte(s)\n", sizeof(long));
+		printf("Size of a long long init %lu\n byte(s)\n", sizeof(long long));
 		printf("Size of a float %lu\n byte(s)\n", sizeof(float));
 			return (0);
 }
