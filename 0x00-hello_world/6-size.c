027037e5ba0b0d214de011c3047b266dc69e0513
Titania792 <darkwarrior792@gmail.com>
2022-02-03 10:16:06 -0800

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		printf("Size of a char %lu\n byte(s)\n", sizeof(char));
+		printf("Size of an int %lu\n byte(s)\n", sizeof(int));
+		printf("Size of a long init %lu\n byte(s)\n", sizeof(long init));
+		printf("Size of a long long init %lu\n byte(s)\n", sizeof(long long init));
+		printf("Size of a float %lu\n byte(s)\n", sizeof(float));
+			return (0);
+}
