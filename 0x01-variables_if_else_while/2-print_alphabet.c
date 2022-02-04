e739bd5ff34c1c3d8e90d274661e2fe28602281c
Titania792 <darkwarrior792@gmail.com>
2022-02-04 10:05:32 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - Entry Point
-*
-* Return: Always 0 (Success)
-*
-*/
+ * main - Entry Point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	char letter;
+	int letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-			    putchar(letter);
-
+	for (letter = 97; letter <= 122; letter++)
+	{
+		putchar(letter);
+	}
 	putchar('\n');
 
-		return (0);
 }
