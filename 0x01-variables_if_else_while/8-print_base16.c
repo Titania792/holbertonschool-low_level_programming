dd47cf9806afa2a15e5e7c3ed5a6458235dbf1f7
Titania792 <darkwarrior792@gmail.com>
2022-02-04 12:12:32 -0800

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int letter;
+
+	for (letter = 48; letter <= 57; letter++)
+	{
+	putchar(letter);
+	}
+	for (letter = 97; letter <= 102; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
