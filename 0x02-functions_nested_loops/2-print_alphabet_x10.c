2753b412d79aef460b1170ea5408bb6d845f6b20
Titania792 <darkwarrior792@gmail.com>
2022-02-07 10:35:50 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* main - Entry Point
-*
-* Return: Always 0 (Success)
+* print_alphabet_x10 - abcd
 */
-int main(void);
 void print_alphabet_x10(void);
 {
 	int letter;
 
 	for (letter = 1; letter <= 10; ++letter)
-	print_alphabet();
 	putchar('\n');
-	return (0);
 }
