5f1db32a27c6811927f02570dfad8941bf535209
Titania792 <darkwarrior792@gmail.com>
2022-02-07 16:31:31 -0800

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* times_table - xdd
+*/
+void times_table(void)
+{
+	int a, b, res = 0;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 10; b++)
+		{
+			res = b * a;
+			if (b != 0)
+			{
+				_putchar(' ');
+				if (res < 10)
+					_putchar(' ');
+			}
+			if (res > 9)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar(res + '0');
+			
+			if (b == 9)
+				_putchar('\n');
+			else
+				_putchar(',');
+		}
+	}
+}
