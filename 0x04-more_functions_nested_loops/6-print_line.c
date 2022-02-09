41cc8c37054e610a5fa40a9171e1a028c885e6fc
Titania792 <darkwarrior792@gmail.com>
2022-02-09 11:09:27 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_line - yuk
+ *@n: sdfx
+ *
+ * Return:fghj
+*/
+void print_line(int n)
+{
+	int count;
+
+	if (n >= 1)
+	{
+		for (count = 0; count < n; count++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
