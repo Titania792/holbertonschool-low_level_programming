6509de045a7fe84ad91a27b37529e6833c3dce34
Titania792 <darkwarrior792@gmail.com>
2022-02-09 12:05:04 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagonal - yuk
+ *@n: sdfx
+ *
+ * Return:fghj
+*/
+void print_diagonal(int n)
+{
+	int x, e;
+
+	for (x = 1; x < n; x++)
+	{
+		for(e = 1; e < x; e++)
+		{
+		_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	if(n >= 1)
+	_putchar('\n');
+}
