4d00047a2ec9655bbc7adbe9c8473c17bf2d7fd5
Titania792 <darkwarrior792@gmail.com>
2022-02-09 06:08:23 -0800

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,16 +12,16 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
 	else
-		(c > a && a > b);
+		(c > a && c > b);
 	{
 		largest = c;
 	}
