8d730caaa5bd9e0dd79b6b54f0aa3671b7d0148e
Titania792 <darkwarrior792@gmail.com>
2022-02-16 05:14:43 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_strncat - function that concatenates two strings.
+*@src: tyui
+*@dest: tyuip
+*@n: rtfg
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0, l = 0;
+
+	while (dest[x++])
+	{
+		l++;
+	}
+	for (x = 0; x < n && src[x] != '\0'; l++, x++)
+	{
+		dest[l] = src[x];
+	}
+
+	return (dest);
+}
