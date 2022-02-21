1c63d3c37d76214f3b6dfecadada4afcdcfb40d5
Titania792 <darkwarrior792@gmail.com>
2022-02-21 08:49:05 -0800

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,13 @@ char *_strchr(char *s, char c)
 {
 	int b;
 
-	for (b = 0; s[b] != c; b++)
+	for (b = 0; s[b] != '\0'; b++)
 	{
 		if (s[b] == c)
 			return (s + b);
-
 	}
+	if (c == '\0')
+		return (s);
+
 	return (0);
 }
