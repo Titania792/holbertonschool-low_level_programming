994b53479a19b3d72fafa474c498d2ecd886b5c2
Titania792 <darkwarrior792@gmail.com>
2022-02-21 07:58:18 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strchr - function that locates a character in a string.
+*@c: character
+*@s: string
+*
+* Return: s
+*/
+char *_strchr(char *s, char c)
+{
+	int b;
+
+	for (b = 0; s[b] != '\0'; b++)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (0);
+}
