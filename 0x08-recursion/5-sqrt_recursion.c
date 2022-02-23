802d9065db8e11b0404416de61d7569d85763cf4
Titania792 <darkwarrior792@gmail.com>
2022-02-23 12:55:46 -0800

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*_con - ghj
+*@n: n° given
+*@y: aaa
+*Return: If n does not have a natural square root,
+*the function should return -1
+*/
+int _con(int y, int n)
+{
+	if (y * y == n)
+		return (y);
+	else if (y * y > n)
+		return (-1);
+	return (_con(n, y + 1));
+}
+
+/**
+*_sqrt_recursion - function that returns the
+*natural square root of a number.
+*@n: n° given
+*Return: If n does not have a natural square root,
+*the function should return -1
+*
+*/
+int _sqrt_recursion(int n)
+{
+		return (_con(n, 0));
+}
