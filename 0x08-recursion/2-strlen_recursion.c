76d5c5f9a2255d3f64114a09d6dbbc32d89a3b17
Titania792 <darkwarrior792@gmail.com>
2022-02-23 09:38:25 -0800

    commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,16 +3,16 @@
 /**
 *_strlen_recursion - function that returns the length of a string.
 *@s: string
-*Return - a
+*Return: 0
 */
 int _strlen_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return(1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
