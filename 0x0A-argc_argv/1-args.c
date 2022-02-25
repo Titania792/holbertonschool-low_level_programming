58da2643f5ac4e52fd188896ce54234a07138aeb
Titania792 <darkwarrior792@gmail.com>
2022-02-25 05:55:58 -0800

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*@argc: argument count
+*@argv: argument variable
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
