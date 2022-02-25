01991a9ac02c78ac8580b7c0849eaa76b2fa55f4
Titania792 <darkwarrior792@gmail.com>
2022-02-25 05:28:51 -0800

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*@argc: argument count inside argv
+*@argv: argument value (array)
+*
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
