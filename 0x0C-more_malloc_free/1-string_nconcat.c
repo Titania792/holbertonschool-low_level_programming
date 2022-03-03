c25468ebd730817f30e213d1349ae1bba3579355
Titania792 <darkwarrior792@gmail.com>
2022-03-03 10:34:33 -0800

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 
 	for (j = 0; j < n && s2[j]; j++)
-		str[i + j] = s2[j];
+		str[i++] = s2[j];
 
 	str[i + j] = '\0';
 
