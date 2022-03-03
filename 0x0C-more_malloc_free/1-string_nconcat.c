d43690c6a20476d3525cd2520f716bb9fc03673d
Titania792 <darkwarrior792@gmail.com>
2022-03-03 09:39:30 -0800

    commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+*string_nconcat - function that concatenates two strings.
+*@s1: string 1
+*@s2: string 2
+*@n: aaa
+*
+*Return: pointer pointing to a newly allocated space in memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int a = 0;
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[a])
+		a++;
+
+	str = malloc(sizeof(str) * (a + n + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < a; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < n && s2[j]; j++)
+	{
+		str[i + j] = s2[j];
+		if (j >= n)
+			return (NULL);
+	}
+	str[i + j] = '\0';
+
+	return (str);
+}
