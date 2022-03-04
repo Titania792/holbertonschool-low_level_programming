54543d4c9ebfa2220d66931b9d6db266250e5c01
Titania792 <darkwarrior792@gmail.com>
2022-03-03 18:45:51 -0800

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,5 +8,15 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = (unsigned int *)calloc(nmemb, size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
 }
