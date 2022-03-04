26f74264061799a3dd759401c1291d77b84fbf18
Titania792 <darkwarrior792@gmail.com>
2022-03-03 16:49:43 -0800

    commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_calloc - function that allocates memory for an array, using malloc.
+*@nmemb: aaa
+*@size: aaa
+*Return: aaa
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*array_range - function that creates an array of integers.
+*@min: minimo
+*@max: maximo
+*Return: Return: the pointer to the newly created array
+*If min > max, return NULL
+*If malloc fails, return NULL
+*
+*/
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, j = max - min;
+	int a = 0;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(sizeof(int) * j + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+		ptr[a++] = i;
+
+	return (ptr);
+}
