8f4b984a7b0816b1d1381a05225ef4e50ac15773
Titania792 <darkwarrior792@gmail.com>
2022-03-08 10:17:46 -0800

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
-#define MACRO __FILE__
 #include <stdio.h>
 
 /**
-*main -program that prints the name of the file it was compiled from, followed by a new line.
-*
+*main - program that prints the name of the file it was compiled from, followed by a new line.
 *
 */
 int main(void)
 {
-	printf("MACRO\n");
+	printf(__FILE__"\n");
 	return (0);
 }
