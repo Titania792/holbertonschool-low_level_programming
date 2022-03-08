109777b810db2f5faaf2b4a138945d509b9e84a6
Titania792 <darkwarrior792@gmail.com>
2022-03-08 13:42:04 -0800

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/**
+*struct dog - jjj
+*@name: aa
+*@age: aa
+*@owner: aa
+*/
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+dog;
+
+#endif
