938da89fe3e196260b2e8cc3ea79ba3bf5ed34d3
Titania792 <darkwarrior792@gmail.com>
2022-03-08 10:18:53 -0800

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
-*main - program that prints the name of the file it was compiled from, followed by a new line.
-*
+*main - program that prints the name of the file it was compiled from,
+*followed by a new line.
+*Return: aa
 */
 int main(void)
 {
