f067a61bd7c404afca4484ce274d778709b00075
Titania792 <darkwarrior792@gmail.com>
2022-03-10 17:01:17 -0800

    igiveup

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+*struct print - aaa
+*@print: aa
+*@ty: aa
+*/
+typedef struct print
+{
+  char print;
+  void(*f)(va_list ty);
+} print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
