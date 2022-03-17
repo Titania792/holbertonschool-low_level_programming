2e99d44064c7c7dd5000ca83262e9347bce91027
Titania792 <darkwarrior792@gmail.com>
2022-03-17 16:52:03 -0700

    100-first.c 17/03

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+*b_main -  function that prints a string before the main function is executed.
+*
+*Auth: Titania792
+*/
+void b_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
