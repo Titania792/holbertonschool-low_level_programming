abd7895d81eb2732350d37be6ef3137a08edc178
Titania792 <darkwarrior792@gmail.com>
2022-03-28 13:09:24 -0700

    1-create_file.c 28/03 ending

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,13 +12,13 @@ int create_file(const char *filename, char *text_content)
 	int wr, fd;
 	unsigned int len = 0;
 
-	len = strlen(text_content);
-
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
+
+	len = strlen(text_content);
 
 	fd = open(filename, O_TRUNC | O_CREAT | O_RDWR, 0600);
 
