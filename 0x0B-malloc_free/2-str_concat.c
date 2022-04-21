19627773a3c136162686f9557c049a03cf25ed73
Titania792 <darkwarrior792@gmail.com>
2022-04-21 09:17:19 -0700

    fixing betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,17 +24,19 @@ char *str_concat(char *s1, char *s2)
 	c = malloc((sizeof(char) * (s + 1)));
 
 	if (c == NULL)
+	{
 		return (NULL);
+	}
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 		{
 			c[i] = s1[i];
 		}
-	for (j = 0; s2[j] != '\0'; j++,  i++)
-        {
-            c[i] = s2[j];
-        }
+		for (j = 0; s2[j] != '\0'; j++,  i++)
+		{
+			c[i] = s2[j];
+		}
 	c[i] = '\0';
 	}
 
