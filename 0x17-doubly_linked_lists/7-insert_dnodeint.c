2083dcea6f385995e360f97baba7c7a9e7feb27a
Titania792 <darkwarrior792@gmail.com>
2022-05-05 13:19:12 -0700

    7 4.0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	currentnode = *h;
 	nextnode = malloc(sizeof(dlistint_t));
 
+	if (h == NULL)
+        return (NULL);
 	if (nextnode == NULL)
 		return (NULL);
 
