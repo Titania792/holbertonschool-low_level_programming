d915b07d07c4a75080f2a2f5de7ce815b7235743
Titania792 <darkwarrior792@gmail.com>
2022-05-05 12:51:08 -0700

    AAAAAAAAAAAAAAAAAA

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -41,6 +41,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	nextnode = currentnode->next;
+	nextnode->prev =currentnode;
 	currentnode->next = nextnode->next;
 	free(nextnode);
 
