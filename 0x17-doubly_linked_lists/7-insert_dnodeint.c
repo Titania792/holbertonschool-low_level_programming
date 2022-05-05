5984292816fa76e555e26cc93a011b349f3868cf
Titania792 <darkwarrior792@gmail.com>
2022-05-05 12:01:52 -0700

    7-insert_dnodeint.c 3.0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		nextnode->next = currentnode;
 		nextnode->prev = NULL;
+		currentnode->prev = nextnode;
 		*h = nextnode;
 		return (*h);
 	}
