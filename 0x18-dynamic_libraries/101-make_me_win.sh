3659c81d841de8af2d6c5d907cdad30acbaaa072
Titania792 <darkwarrior792@gmail.com>
2022-05-09 12:30:36 -0700

    try 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-echo "int srand(void *p){return (0);}int idx = 0;int rand(void){int list[] = {9, 8, 10, 24, 75, 9};idx++;return (list[idx]);}" > /root/injected.c
-gcc -fPIC --shared /root/injected.c -o /root/injectedlib.so
-export LD_PRELOAD=/root/injectedlib.so
