e30d62bef2ff1c862c3550d36ea005cafa13217b
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-05-26 11:17:27 -0300

    Setting and task 0

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - hash function
+ * @key: key to hash
+ * @size: size of the hash table
+ *
+ * Return: index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value of a key
+ * @ht: hash table
+ * @key: key to get
+ *
+ * Return: value of the key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+}
