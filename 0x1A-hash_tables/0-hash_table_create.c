a534642c2e5135b00512e91606acfc01747e20f3
Titania792 <darkwarrior792@gmail.com>
2022-05-27 04:22:38 -0700

    betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * key_index - hash function
- * @key: key to hash
+ * hash_table_create - hash function
  * @size: size of the hash table
  *
  * Return: index of the key
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
-    unsigned long int i;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return (NULL);
-    hash_table->size = size;
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return (NULL);
-    }
-    for (i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
-    return (hash_table);
+	if (size == 0)
+		return (NULL);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
